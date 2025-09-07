2f174310f0fb44ddea56a1d468d1bb376688c2d5
zyoung71 <zyoung0071@gmail.com>
2025-09-07 16:18:03 -0400

    module typecheck

--- include/Module.h
+++ include/Module.h
@@ -1,15 +1,44 @@
 #pragma once
 
+#include <vector>
+
 namespace uazips
 {
 
     class Module
     {
     public:
-        Module() {}
+        static std::vector<const Module*> InstalledModules;
+
+    public:
+        inline Module()
+        {
+            InstalledModules.push_back(this);
+        }
         virtual ~Module() {}
+
+        template<class Mod, typename InType>
+        static bool IsModuleAType(const InType&& module)
+        {
+            if constexpr !(std::is_pointer_v<std::decay_t<InType>>)
+                IsModuleAType<Mod>(module);
+            else
+                return dynamic_cast<Mod*>(module) != nullptr;
+        }
+
+        template<class Mod>
+        static std::vector<Mod> GetModulesByType()
+        {
+            std::vector<Mod*> modules;
+            for (auto&& mod : InstalledModules)
+                if (IsModuleAType<Mod>(mod))
+                    modules.push_back(mod);
+            return modules;
+        }
 
         virtual void Initialize() = 0;
     };
+
+    std::vector<const Module*> Module::InstalledModules = {};
 
 }
